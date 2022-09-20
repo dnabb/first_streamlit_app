8ec4ebe8e440ebecefe340f8f3cb3bc027f2c4f9
Daniele <d_abbatelli@hotmail.com>
2022-09-20 15:31:05 +0200

    Added few lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('This is a title')
+
+streamlit.header('This is a header')
+streamlit.text('This is a text')
+streamlit.text('This is another text')
+streamlit.text('This is yet another text')
