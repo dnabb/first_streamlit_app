b87ea76475636dc7ee65d8ed91c7c08d8d329c43
Daniele <d_abbatelli@hotmail.com>
2022-09-20 15:33:41 +0200

    Added a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('This is a title')
 
@@ -8,3 +9,6 @@ streamlit.text('This is another text')
 streamlit.text('This is yet another text with emojis 🥣 🥗 🐔 🥑🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
