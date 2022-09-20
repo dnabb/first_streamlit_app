35c4e0f3b74edacb61502524d7c311f7fd40e673
Daniele <d_abbatelli@hotmail.com>
2022-09-20 15:32:37 +0200

    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.title('This is a title')
 streamlit.header('This is a header')
 streamlit.text('This is a text')
 streamlit.text('This is another text')
-streamlit.text('This is yet another text')
+streamlit.text('This is yet another text with emojis 🥣 🥗 🐔 🥑🍞')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
